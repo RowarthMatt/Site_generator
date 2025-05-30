44ebfad6cd3943a637d03dcecaf2bdfb3cf50b59
RowarthMatt <rowarthmatt@gmail.com>
2025-05-30 09:03:03 +0100

    CH3 L5

--- src/main.py
+++ src/main.py
@@ -3,9 +3,23 @@ from htmlnode import *
 from inline_markdown import *
 
 def main():
-    node = TextNode("This is text with a `code block` word", TextType.TEXT)
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    link_node = TextNode(
+        "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev). Thanks!",
+        TextType.TEXT,
+    )
 
-    print(new_nodes)
+    image_node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
 
+    new_link_nodes = split_nodes_link([link_node])
+    new_image_nodes = split_nodes_image([image_node])
+
+    print(f'{link_node}\n---------------\n')
+    for node in new_link_nodes:
+        print(node)
+    print(f'\n\n{image_node}\n---------------\n')
+    for node in new_image_nodes:
+        print(node)
 main()
